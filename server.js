1da9f5f158950d98c267d3e71572c65aaa82bc2d
Michael Kornblum <mkornblum71@gmail.com>
2019-08-10 11:53:55 -0500

    revert to non PWA

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: ture }));
+//app.use(enforce.HTTPS({ trustProtoHeader: ture }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
@@ -29,9 +29,9 @@ app.listen(port, error => {
 	console.log("Server running on port " + port);
 });
 
-app.get("/service-worker.js", (req, res) =>
-	res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"))
-);
+// app.get("/service-worker.js", (req, res) =>
+// 	res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"))
+// );
 
 app.post("/payment", (req, res) => {
 	const body = {
